c81066905114e536a10abc922aaaa8a81bad050d
suakang17 <rkdtndk09@gmail.com>
2022-06-27 16:59:40 +0900

    init:file structure

---
+++ bulletin/src/main/java/com/example/bulletin/BulletinApplication.java
@@ -0,0 +1,13 @@
+package com.example.bulletin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinApplication.class, args);
+	}
+
+}
